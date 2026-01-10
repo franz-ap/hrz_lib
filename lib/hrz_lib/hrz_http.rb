c91dab3d5f1bbce1e04997fffc91a53c74b52f00
Franz Apeltauer <f.ap@wolke7.net>
2026-01-10 13:24:21 +0100

    Extended http_request.

--- lib/hrz_lib/hrz_http.rb
+++ lib/hrz_lib/hrz_http.rb
@@ -23,19 +23,53 @@ module HrzLib
   class HrzHttp
 
     # Perform HTTP request.
-    # Returns a [Hash]:
-    #   :q_ok [Boolean] ... true:  Success. The result body is in :body
-    #                       false: Error, problems. :body is empty or at least unusable. Error messages were already issued.
-    #   :body [String] .... The body of the http response.
-    def self.http_request(b_url,                # URL to be called. nil or empty is ok, will return ok and an empty body.
+    # @param b_url       [String]      URL to be called. nil or empty is ok, will return q_ok=false and an empty body.
+    # @param b_method    [String]      Method to be used: 'GET', 'PUT', 'POST', ... Optional.
+    # @param aux_hdr     [Array, Hash] Auxiliary HTTP header lines, optional:
+    #                                  In an Array:  [ {key: 'aux_key1', val: 'aux_value1'}, {key: 'aux_key2', val: 'aux_value2'} ]
+    #                                  In a Hash:    {        aux_key1:        aux_value1,          aux_key2:        aux_value2   }
+    # @param b_post_data [String]      Data to be sent in the request body of a POST. Optional.
+    # @param b_name_svc  [String]      "Human readable name" of service to be called. For messages. Optional.
+    # @param arr_resp_ok [Array of Integer] HTTP response codes, that should be taken as ok, if the standard 200 is not enough.
+    # @return [Hash]:
+    #   :q_ok [Boolean] ... true:  Success, HTTP request done. The result body is in :body
+    #                       false: Error, problems or there was no URL, i.e. nothing to do. :body is empty or at least unusable. Error messages were already issued.
+    #   :body [String] .... The body of the HTTP response.
+    #
+    # Examples:
+    #   a) aux_hdr Array
+    #   hsh_res = HrzLib::HrzHttp.http_request('https://redm.xy/info.json',
+    #                                          'GET',
+    #                                          [{key: 'X-Redmine-API-Key', val: api_key},
+    #                                           {key: 'Content-Type',      val: 'application/json'}
+    #                                          ],
+    #                                          nil,
+    #                                          'instance_info')
+    #   if hsh_res[:q_ok]
+    #     data = JSON.parse(hsh_res[:body])
+    #     ...
+    #   end
+    #
+    #   b) aux_hdr Hash
+    #   hsh_res = HrzLib::HrzHttp.http_request('https://redm.xy/info.json',
+    #                                          'GET',
+    #                                          {'X-Redmine-API-Key' => api_key, 'Content-Type' => 'application/json'},
+    #                                          nil,
+    #                                          'instance_info')
+    #   if hsh_res[:q_ok]
+    #     data = JSON.parse(hsh_res[:body])
+    #     ...
+    #   end
+    def self.http_request(b_url,                # URL to be called. nil or empty is ok, will return q_ok=false and an empty body.
                           b_method     = 'GET', # Method to be used: 'GET', 'PUT', 'POST', ...
-                          arr_aux_hdr  = [],    # Array of auxiliary HTTP header lines: {key: 'aux_key1', val: 'aux_value1'}
-                          b_post_data  = '',    # Data to be sent in the request body of a POST.             Optional.
-                          b_name_svc   = '')    # Human readable name of service to be called. For messages. Optional.
+                          aux_hdr      = [],    # Array of auxiliary HTTP header lines: {key: 'aux_key1', val: 'aux_value1'}
+                          b_post_data  = '',    # Data to be sent in the request body of a POST.               Optional.
+                          b_name_svc   = '',    # "Human readable name" of service to be called. For messages. Optional.
+                          arr_resp_ok  = [200]) # HTTP response codes, that should be taken as ok.
       HrzLogger.debug_msg "HrzHttp.http_request: #{b_method.to_s} #{b_url.to_s}" + (b_name_svc.empty? ? '' : ' / Service ') + b_name_svc
       t_start = Time.now
       hsh_result = { q_ok: true, body: '' }
-      return   if b_url.nil? || b_url.empty?
+      return { q_ok: false, body: '' }  if b_url.nil? || b_url.empty?
       begin
         uri  = URI.parse(b_url)
         http = Net::HTTP.new(uri.host, uri.port)
@@ -58,16 +92,25 @@ module HrzLib
         end
         if ! request.nil?
           # Auxiliary HTTP header lines
-          for aux_hdr in arr_aux_hdr
-            request[ aux_hdr[:key] ] = aux_hdr[:val]
+          if aux_hdr.is_a?(Array)
+            for aux_hdr1 in aux_hdr
+              request[ aux_hdr1[:key] ] = aux_hdr1[:val]
+            end
+          elsif aux_hdr.is_a?(Hash)
+            request.merge!(aux_hdr)
+          else
+            HrzLogger.debug_msg "HrzHttp.http_request: Unsupported object type  #{aux_hdr.class.name} of aux_hdr. Ignoring it."
           end
           # Send the request
           response = http.request(request)
           # Check the answer
-          if response.code == '200'
-             hsh_result[:body] = response.body
+          if arr_resp_ok.include?(response.code.to_i)
+            # Ok
+            hsh_result[:body] = response.body
           else
-            HrzLogger.error_msg "Request" + (b_name_svc.empty? ? '' : ' for service ') +  b_name_svc + " failed with HTTP code: #{response.code}  URL: #{b_url} Result: #{response.body}"
+            HrzLogger.error_msg "Request" + (b_name_svc.empty? ? '' : ' for service ') +  b_name_svc + " failed with HTTP code: #{response.code} - #{response.message}.  URL: #{b_url} Result: #{response.body}"
+            # Response herader:
+            response.each_header { |key, value| HrzLogger.debug_msg "  #{key}: #{value}" }
             hsh_result[:q_ok] = false
           end
         end
